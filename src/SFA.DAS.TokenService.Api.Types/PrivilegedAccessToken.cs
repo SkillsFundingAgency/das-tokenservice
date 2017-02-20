256874331df96e69b1fdb163944ec6dbb343e1df
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-20 14:20:19 +0000

    Add controller and type placeholders

---
+++ src/SFA.DAS.TokenService.Api.Types/PrivilegedAccessToken.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SFA.DAS.TokenService.Api.Types
+{
+    public class PrivilegedAccessToken
+    {
+        public string AccessCode { get; set; }
+        public DateTime ExpiryTime { get; set; }
+    }
+}
---
+++ src/SFA.DAS.TokenService.Api/Controllers/PrivilegedAccessController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using SFA.DAS.TokenService.Api.Types;
+
+namespace SFA.DAS.TokenService.Api.Controllers
+{
+    [RoutePrefix("api/PrivilegedAccess")]
+    public class PrivilegedAccessController : ApiController
+    {
+        [HttpGet]
+        [Route("", Name = "GetPrivilegedAccessToken")]
+        public async Task<IHttpActionResult> GetPrivilegedAccessToken()
+        {
+            await Task.Delay(1);
+
+            return Ok(new PrivilegedAccessToken
+            {
+                AccessCode = "ABC123",
+                ExpiryTime = DateTime.Now.AddHours(4)
+            });
+        }
+    }
+}
