9c501fce68d7c8ca2e3ea4cad5edf077d889adec
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-21 10:58:31 +0000

    Add dependency resolution

---
+++ src/SFA.DAS.TokenService.Api/DependencyResolution/DefaultRegistry.cs
@@ -0,0 +1,53 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultRegistry.cs" company="Web Advanced">
+// Copyright 2012 Web Advanced (www.webadvanced.com)
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+using MediatR;
+using SFA.DAS.TokenService.Infrastructure.Configuration;
+using StructureMap.Configuration.DSL;
+using StructureMap.Graph;
+
+namespace SFA.DAS.TokenService.Api.DependencyResolution
+{
+
+    public class DefaultRegistry : Registry
+    {
+        private const string ServiceNamespace = "SFA.DAS";
+
+        public DefaultRegistry()
+        {
+            Scan(scan =>
+            {
+                scan.AssembliesFromApplicationBaseDirectory(a => a.GetName().Name.StartsWith(ServiceNamespace));
+                scan.RegisterConcreteTypesAgainstTheFirstInterface();
+            });
+
+            RegisterMediator();
+            RegisterConfiguration();
+        }
+
+        private void RegisterMediator()
+        {
+            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+            For<IMediator>().Use<Mediator>();
+        }
+        private void RegisterConfiguration()
+        {
+            For<KeyVaultConfiguration>().Use(() => new KeyVaultConfiguration());
+            For<OAuthTokenServiceConfiguration>().Use(() => new OAuthTokenServiceConfiguration());
+        }
+    }
+}
