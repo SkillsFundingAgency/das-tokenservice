1f9eb156c18896076fc5350446f29c7a4a58822b
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-21 08:36:11 +0000

    Remove unused namespaces

--- src/SFA.DAS.TokenService.Application/PrivilegedAccess/GetPrivilegedAccessToken/PrivilegedAccessQuery.cs
+++ src/SFA.DAS.TokenService.Application/PrivilegedAccess/GetPrivilegedAccessToken/PrivilegedAccessQuery.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using MediatR;
+﻿using MediatR;
 using SFA.DAS.TokenService.Domain;
 
 namespace SFA.DAS.TokenService.Application.PrivilegedAccess.GetPrivilegedAccessToken
--- src/SFA.DAS.TokenService.Domain/OAuthAccessToken.cs
+++ src/SFA.DAS.TokenService.Domain/OAuthAccessToken.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SFA.DAS.TokenService.Domain
+﻿namespace SFA.DAS.TokenService.Domain
 {
     public class OAuthAccessToken
     {
