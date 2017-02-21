e16ad381b3a0e52b20fafabd5a90c85a7feb66df
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-21 10:40:47 +0000

    Implement OAuthTokenService

---
+++ src/SFA.DAS.TokenService.Infrastructure.UnitTests/Services/OAuthTokenServiceTests/WhenGettingAccessToken.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.TokenService.Infrastructure.Configuration;
+using SFA.DAS.TokenService.Infrastructure.Http;
+using SFA.DAS.TokenService.Infrastructure.Services;
+
+namespace SFA.DAS.TokenService.Infrastructure.UnitTests.Services.OAuthTokenServiceTests
+{
+    public class WhenGettingAccessToken
+    {
+        private const string AccessToken = "ACCESS-TOKEN";
+        private const string RefreshToken = "REFRESH-TOKEN";
+        private const int ExpiresIn = 123;
+        private const string Scopes = "SCOPES";
+        private const string TokenType = "TOKEN-TYPE";
+        private const string ClientSecret = "SECRET";
+        private const string ClientId = "CLIENT-ID";
+
+        private Mock<IHttpClientWrapper> _httpClient;
+        private List<MediaTypeWithQualityHeaderValue> _clientAcceptHeaders;
+        private OAuthTokenServiceConfiguration _configuration;
+        private OAuthTokenService _service;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _configuration = new OAuthTokenServiceConfiguration
+            {
+                Url = "http://unit.test/token",
+                ClientId = ClientId
+            };
+
+            _clientAcceptHeaders = new List<MediaTypeWithQualityHeaderValue>();
+            _httpClient = new Mock<IHttpClientWrapper>();
+            _httpClient.Setup(c => c.AcceptHeaders)
+                .Returns(_clientAcceptHeaders);
+            _httpClient.Setup(c => c.Post<OAuthTokenResponse>(_configuration.Url, It.IsAny<OAuthTokenRequest>()))
+                .ReturnsAsync(new OAuthTokenResponse
+                {
+                    AccessToken = AccessToken,
+                    RefreshToken = RefreshToken,
+                    ExpiresIn = ExpiresIn,
+                    Scope = Scopes,
+                    TokenType = TokenType
+                });
+
+            _service = new OAuthTokenService(_httpClient.Object, _configuration);
+        }
+
+        [Test]
+        public async Task ThenItShouldReturnTheAccessTokenFromTheHttpClient()
+        {
+            // Act
+            var actual = await _service.GetAccessToken(ClientSecret);
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(AccessToken, actual.AccessToken);
+            Assert.AreEqual(RefreshToken, actual.RefreshToken);
+            Assert.AreEqual(ExpiresIn, actual.ExpiresIn);
+            Assert.AreEqual(Scopes, actual.Scope);
+            Assert.AreEqual(TokenType, actual.TokenType);
+        }
+
+        [Test]
+        public void ThenItShouldSetTheAcceptHeadersToHmrcJson()
+        {
+            // Assert
+            Assert.AreEqual(1, _clientAcceptHeaders.Count);
+            Assert.AreEqual("application/vnd.hmrc.1.0+json", _clientAcceptHeaders[0].MediaType);
+        }
+
+        [Test]
+        public async Task ThenItShouldUseCorrectRequestParameters()
+        {
+            // Act
+            await _service.GetAccessToken(ClientSecret);
+
+            // Assert
+            _httpClient.Verify(c => c.Post<OAuthTokenResponse>(_configuration.Url, It.Is<OAuthTokenRequest>(r => r.ClientId == ClientId
+                                                                                                              && r.ClientSecret == ClientSecret
+                                                                                                              && r.GrantType == "client_credentials"
+                                                                                                              && r.Scopes == "read:apprenticeship-levy")), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.TokenService.Infrastructure/Services/OAuthTokenService.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.TokenService.Domain;
+using SFA.DAS.TokenService.Domain.Services;
+using SFA.DAS.TokenService.Infrastructure.Configuration;
+using SFA.DAS.TokenService.Infrastructure.Http;
+
+namespace SFA.DAS.TokenService.Infrastructure.Services
+{
+    public class OAuthTokenService : IOAuthTokenService
+    {
+        private readonly IHttpClientWrapper _httpClient;
+        private readonly OAuthTokenServiceConfiguration _configuration;
+
+        public OAuthTokenService(IHttpClientWrapper httpClient, OAuthTokenServiceConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+
+            _httpClient.AcceptHeaders.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.hmrc.1.0+json"));
+        }
+
+        public async Task<OAuthAccessToken> GetAccessToken(string clientSecret)
+        {
+            var request = new OAuthTokenRequest
+            {
+                ClientId = _configuration.ClientId,
+                ClientSecret = clientSecret,
+                GrantType = "client_credentials",
+                Scopes = "read:apprenticeship-levy"
+            };
+            var hmrcToken = await _httpClient.Post<OAuthTokenResponse>(_configuration.Url, request);
+            return new OAuthAccessToken
+            {
+                AccessToken = hmrcToken.AccessToken,
+                RefreshToken = hmrcToken.RefreshToken,
+                ExpiresIn = hmrcToken.ExpiresIn,
+                Scope = hmrcToken.Scope,
+                TokenType = hmrcToken.TokenType
+            };
+        }
+    }
+}
