7f6f28723ee806d979672712ff74928e4533e85e
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-22 08:40:54 +0000

    Add api client

---
+++ src/SFA.DAS.TokenService.Api.Client/ISecureHttpClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SFA.DAS.TokenService.Api.Client
+{
+    internal interface ISecureHttpClient
+    {
+        Task<string> GetAsync(string url);
+    }
+}
---
+++ src/SFA.DAS.TokenService.Api.Client/SecureHttpClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace SFA.DAS.TokenService.Api.Client
+{
+    internal class SecureHttpClient : ISecureHttpClient
+    {
+        private readonly ITokenServiceApiClientConfiguration _configuration;
+
+        public SecureHttpClient(ITokenServiceApiClientConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<string> GetAsync(string url)
+        {
+            var authenticationResult = await GetAuthenticationResult(_configuration.ClientId, _configuration.ClientSecret, _configuration.IdentifierUri, _configuration.Tenant);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private async Task<AuthenticationResult> GetAuthenticationResult(string clientId, string appKey, string resourceId, string tenant)
+        {
+            var authority = $"https://login.microsoftonline.com/{tenant}";
+            var clientCredential = new ClientCredential(clientId, appKey);
+            var context = new AuthenticationContext(authority, true);
+            var result = await context.AcquireTokenAsync(resourceId, clientCredential);
+            return result;
+        }
+    }
+}
