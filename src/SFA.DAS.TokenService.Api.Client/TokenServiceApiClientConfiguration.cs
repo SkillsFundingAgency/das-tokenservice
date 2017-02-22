08e1479c01762b9a805645b1cca533c2e75c4c4c
Simon Bull <simonbu11@users.noreply.github.com>
2017-02-22 14:46:34 +0000

    Add default implementation od client configuration

---
+++ src/SFA.DAS.TokenService.Api.Client/TokenServiceApiClientConfiguration.cs
@@ -0,0 +1,11 @@
+namespace SFA.DAS.TokenService.Api.Client
+{
+    public class TokenServiceApiClientConfiguration : ITokenServiceApiClientConfiguration
+    {
+        public string ApiBaseUrl { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string IdentifierUri { get; set; }
+        public string Tenant { get; set; }
+    }
+}
