44d68c05cc0dec4c72b8dd60048838ec06c22e31
Ian Gledhill <ian@gledhills.mobi>
2018-04-23 15:37:41 +0100

    Modify startup to persist in getting a token until HMRC reports success

---
+++ src/SFA.DAS.TokenService.Application/PrivilegedAccess/TokenRefresh/HmrcAuthTokenBrokerConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SFA.DAS.TokenService.Application.PrivilegedAccess.TokenRefresh
+{
+    public class HmrcAuthTokenBrokerConfig : IHmrcAuthTokenBrokerConfig
+    {
+        public HmrcAuthTokenBrokerConfig()
+        {
+            RetryDelay = TimeSpan.FromSeconds(30);    
+        }
+
+        public TimeSpan RetryDelay { get; set; }
+    }
+}
