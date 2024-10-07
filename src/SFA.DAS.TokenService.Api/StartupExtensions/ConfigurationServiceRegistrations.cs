ee3c4ecce07321ed3b258169a39774038a075ede
Jon Harris <jon.harris@fuitsu.com>
2024-10-07 11:34:30 +0100

    Fixing config wireup

--- src/SFA.DAS.TokenService.Api/StartupExtensions/ConfigurationServiceRegistrations.cs
+++ src/SFA.DAS.TokenService.Api/StartupExtensions/ConfigurationServiceRegistrations.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Options;
+using SFA.DAS.TokenService.Domain.Data;
 using SFA.DAS.TokenService.Infrastructure.Configuration;
+using SFA.DAS.TokenService.Infrastructure.Data;
 
 namespace SFA.DAS.TokenService.Api.StartupExtensions;
 
@@ -10,15 +12,21 @@ public static class ConfigurationServiceRegistrations
     public static IServiceCollection AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddOptions();
-        
-        services.Configure<KeyVaultConfiguration>(configuration);
-        services.AddSingleton(cfg => cfg.GetService<IOptions<KeyVaultConfiguration>>()!.Value);
 
-        services.Configure<OAuthTokenServiceConfiguration>(configuration);
-        services.AddSingleton(cfg => cfg.GetService<IOptions<OAuthTokenServiceConfiguration>>()!.Value);
+        services.AddSingleton(new KeyVaultConfiguration
+        {
+            VaultUri = configuration["KeyVaultUri"]
+        });
+
+        services.AddSingleton(new OAuthTokenServiceConfiguration
+        {
+            Url = configuration["HmrcTokenUri"],
+            ClientId = configuration["HmrcTokenClientId"],
+            ClientSecret = configuration["HmrcTokenSecret"]
+        });
 
         services.AddSingleton(configuration);
-        
+
         return services;
     }
 }
