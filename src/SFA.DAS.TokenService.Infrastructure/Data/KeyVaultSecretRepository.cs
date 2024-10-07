9b2bde737ebae6966197834868347feda74d0f2f
Jon Harris <jon.harris@fuitsu.com>
2024-10-07 15:00:21 +0100

    Refactor

--- src/SFA.DAS.TokenService.Infrastructure/Data/KeyVaultSecretRepository.cs
+++ src/SFA.DAS.TokenService.Infrastructure/Data/KeyVaultSecretRepository.cs
@@ -12,22 +12,22 @@ public class KeyVaultSecretRepository(KeyVaultConfiguration configuration, ILogg
     public async Task<string> GetSecretAsync(string name)
     {
         logger.LogInformation("Getting secret {Name} from KeyVault with Uri '{Uri}' using Managed Service Identity", name, configuration.VaultUri);
+        
+        var vaultUri = new Uri(configuration.VaultUri ?? throw new InvalidOperationException("KeyVault Uri is null"));
+        
+        var options = new SecretClientOptions
+        {
+            Retry =
+            {
+                Delay= TimeSpan.FromSeconds(2),
+                MaxDelay = TimeSpan.FromSeconds(16),
+                MaxRetries = 5,
+                Mode = RetryMode.Exponential
+            }
+        };
 
         try
         {
-            var vaultUri = new Uri(configuration.VaultUri ?? throw new InvalidOperationException("KeyVault Uri is null"));
-            
-            var options = new SecretClientOptions
-            {
-                Retry =
-                {
-                    Delay= TimeSpan.FromSeconds(2),
-                    MaxDelay = TimeSpan.FromSeconds(16),
-                    MaxRetries = 5,
-                    Mode = RetryMode.Exponential
-                }
-            };
-            
             var client = new SecretClient(vaultUri, new DefaultAzureCredential(), options);
         
             var response = await client.GetSecretAsync(name);
--- src/SFA.DAS.TokenService.Infrastructure/Http/HttpClientWrapper.cs
+++ src/SFA.DAS.TokenService.Infrastructure/Http/HttpClientWrapper.cs
@@ -11,13 +11,16 @@ public class HttpClientWrapper : IHttpClientWrapper
     public async Task<T?> Post<T>(string? url, object content)
     {
         using var client = CreateClient();
+        
         var jsonContent = JsonConvert.SerializeObject(content);
         var response = await client.PostAsync(url, new StringContent(jsonContent, Encoding.UTF8, "application/json"));
         var responseBody = await response.Content.ReadAsStringAsync();
+        
         if (!response.IsSuccessStatusCode)
         {
             throw new HttpRequestException($"Http status code {response.StatusCode} indicates failure. (Status description: {response.ReasonPhrase}, body: {responseBody})");
         }
+        
         return JsonConvert.DeserializeObject<T>(responseBody);
     }
 
