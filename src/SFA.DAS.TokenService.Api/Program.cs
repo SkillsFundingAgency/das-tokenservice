42feea575d7e84443b54494079a2ca443ff23950
Jon Harris <jon.harris@fuitsu.com>
2024-10-08 11:40:20 +0100

    Auth/authorization enabling

--- src/SFA.DAS.TokenService.Api/Program.cs
+++ src/SFA.DAS.TokenService.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging.ApplicationInsights;
+using SFA.DAS.TokenService.Api.Authorization;
 using SFA.DAS.TokenService.Api.StartupExtensions;
 using SFA.DAS.TokenService.Application.PrivilegedAccess.GetPrivilegedAccessToken;
 using SFA.DAS.TokenService.Application.PrivilegedAccess.TokenRefresh;
@@ -13,20 +14,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<PrivilegedAccessQuery>());
 builder.Services.AddConfigurationOptions(builder.Configuration);
+
 builder.Services.AddLogging(loggingBuilder =>
 {
     loggingBuilder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
     loggingBuilder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
 });
+
 builder.Services.AddActiveDirectoryAuthentication(builder.Configuration);
 builder.Services.AddApplicationServices();
 builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddControllers();
 
-// TODO wire up authentication/authorization
+// builder.Services.AddApiAuthentication(builder.Configuration);
+// builder.Services.AddApiAuthorization();
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.UseHttpsRedirection();
