e109071f5ef43d2c8ff72c325f7e07143f393982
Jon Harris <jon.harris@fuitsu.com>
2024-11-06 14:37:26 +0000

    Adding logging of all HttpRequests to the HMRC Api

--- src/SFA.DAS.TokenService.Infrastructure/Http/HttpClientWrapper.cs
+++ src/SFA.DAS.TokenService.Infrastructure/Http/HttpClientWrapper.cs
@@ -1,22 +1,26 @@
 ﻿using System.Net.Http.Headers;
 using System.Text;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace SFA.DAS.TokenService.Infrastructure.Http;
 
-public class HttpClientWrapper : IHttpClientWrapper
+public class HttpClientWrapper (ILogger<HttpClientWrapper> logger) : IHttpClientWrapper
 {
     public async Task<T?> Post<T>(string? url, object content)
     {
         using var client = CreateClient();
 
         var jsonContent = JsonConvert.SerializeObject(content);
+        
+        logger.LogWarning("HttpClientWrapper. POST-ing request: {Content} to Uri: {Uri}", jsonContent, url);
+        
         var response = await client.PostAsync(url, new StringContent(jsonContent, Encoding.UTF8, "application/json"));
         var responseBody = await response.Content.ReadAsStringAsync();
 
         if (!response.IsSuccessStatusCode)
         {
-            throw new HttpRequestException($"Http status code {response.StatusCode} indicates failure. (Status description: {response.ReasonPhrase}, body: {responseBody}, content: {JsonConvert.SerializeObject(content)})");
+            throw new HttpRequestException($"Http status code {response.StatusCode} indicates failure. (Status description: {response.ReasonPhrase}, body: {responseBody}, content: {jsonContent})");
         }
 
         return JsonConvert.DeserializeObject<T>(responseBody);
